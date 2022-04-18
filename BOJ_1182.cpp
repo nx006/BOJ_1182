aa9d90d47a4f9e3c2d703b080db8479ef45f4a48
Lee Junseon <limazero14@gmail.com>
2022-04-19 03:31:46 +0900

    초기 커밋.

---
+++ BOJ_1182.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int sizeArr;
+int target;
+int result;
+
+void bricks(int index, int sum, vector<int>& arr)
+{
+	if (index == sizeArr) return;
+	if (sum + arr[index] == target) result++;
+
+	bricks(index + 1, sum + arr[index], arr); //현재 index 숫자를 더하는 경우
+	bricks(index + 1, sum, arr); //현재 index 숫자를 더하지 않고 그냥 넘어가는 경우
+}
+
+int main()
+{
+	
+	cin >> sizeArr >> target;
+	vector<int> arr(sizeArr);
+	for (int i = 0; i < sizeArr; i++) {
+		cin >> arr[i];
+	}
+
+	bricks(0, 0, arr);
+	cout << result;
+	return 0;
+}
